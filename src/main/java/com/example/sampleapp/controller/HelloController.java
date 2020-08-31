e1e32f42bc129dd8f15aa6a935c223c7cb6b19fc
Anand Sinha <anandkumar.sinha@conduent.com>
2020-08-31 13:02:11 +0530

    Simple GET Api : Hello

--- src/main/java/com/example/sampleapp/controller/HelloController.java
+++ src/main/java/com/example/sampleapp/controller/HelloController.java
@@ -4,9 +4,12 @@ import com.example.sampleapp.dto.HelloRequest;
 import com.example.sampleapp.entity.Hello;
 import com.example.sampleapp.repository.HelloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -16,8 +19,13 @@ public class HelloController {
 
     @PostMapping("/writeHello")
     public Hello writeHello(@RequestBody HelloRequest helloRequest){
-        
+
         return helloRepository.save(helloRequest.getHello());
+    }
+
+    @GetMapping("/readHello")
+    public List<Hello> readHello(){
+        return helloRepository.findAll();
     }
 
 }
