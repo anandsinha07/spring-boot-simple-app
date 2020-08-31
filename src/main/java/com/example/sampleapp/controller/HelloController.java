5eb35df238c45378e6ea7ae7f65e84a20f1972c7
Anand Sinha <anandkumar.sinha@conduent.com>
2020-08-31 13:12:46 +0530

    ResponseStatus 200 OK to 201 CREATED

--- src/main/java/com/example/sampleapp/controller/HelloController.java
+++ src/main/java/com/example/sampleapp/controller/HelloController.java
@@ -4,10 +4,8 @@ import com.example.sampleapp.dto.HelloRequest;
 import com.example.sampleapp.entity.Hello;
 import com.example.sampleapp.repository.HelloRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +16,7 @@ public class HelloController {
     private HelloRepository helloRepository;
 
     @PostMapping("/writeHello")
+    @ResponseStatus(HttpStatus.CREATED)
     public Hello writeHello(@RequestBody HelloRequest helloRequest){
 
         return helloRepository.save(helloRequest.getHello());
